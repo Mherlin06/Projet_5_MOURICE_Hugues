a58aa1d8c423373321a8fe32c4aebc1392d54de1
Hugues Mourice <mourice.hugues@gmail.com>
2022-09-15 21:50:07 +0200

    update createProductCard()

--- front/js/script.js
+++ front/js/script.js
@@ -7,17 +7,34 @@ const urlApi = "http://localhost:3000/api/products/";
 const getAllProducts = () => {
   fetch(urlApi)
     .then((response) => response.json())
-    .then((products) => {
-      for (let product of products) {
-        items.innerHTML += `<a href="./product.html?id=${product._id}">
-                                <article>
-                                    <img src=${product.imageUrl} alt=${product.altTxt}>
-                                    <h3 class="productName">${product.name}</h3>
-                                    <p class="productDescription">${product.description}</p>
-                                </article>
-                            </a>`;
-      }
+    .then((dataProducts) => {
+      dataProducts.forEach((product) => {
+        createProductCard(product);
+      });
     });
 };
 
 getAllProducts();
+
+const createProductCard = (product) => {
+  // Create Elements
+  let itemAnchor = document.createElement("a");
+  let itemArticle = document.createElement("article");
+  let itemImg = document.createElement("img");
+  let itemName = document.createElement("h3");
+  let itemDescription = document.createElement("p");
+
+  // Appends Elements
+  items.appendChild(itemAnchor);
+  itemAnchor.appendChild(itemArticle);
+  itemArticle.appendChild(itemImg);
+  itemArticle.appendChild(itemName);
+  itemArticle.appendChild(itemDescription);
+
+  // display product's values
+  itemAnchor.setAttribute("href", "product.html?id=" + product._id);
+  itemImg.setAttribute("src", product.imageUrl);
+  itemImg.setAttribute("alt", product.altTxt);
+  itemName.textContent = product.name;
+  itemDescription.textContent = product.description;
+};
